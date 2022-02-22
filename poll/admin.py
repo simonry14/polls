a2b53b145b410ca1cac23fac34435cafd983fa65
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2022-02-22 15:37:31 +0300

    mya

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Poll, Vote, Choice
 
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(Vote)
+admin.site.register(Choice)
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Poll, Vote, Choice
+from django.http import JsonResponse
 
 # Create your views here.
+
+def polls_list(request):
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+    return JsonResponse(data)
+    #context = {'polls', polls}
+    #return render (request, 'polls.html', context)
+
+def polls_detail(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {"results": {
+        "question": poll.question,
+        "created_by": poll.created_by.username,
+        "pub_date": poll.pub_date
+    }}
+    return JsonResponse(data)
+    #poll = Poll.objects.get(id = pk)
+    #context = {'poll', poll}
+    #return render (request, 'polls-detail.html', context)
+    
